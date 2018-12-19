26f5b0b3ce7bb96e982432ccf4ac101ea461150d
warukira <dwarukira@gmail.com>
2018-12-19 23:44:05 +0300

    add login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './Pages/Dashboard';
+import WrappedLoginForm from './Pages/Login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard />
+        <WrappedLoginForm />
       </div>
     );
   }
